3126e566c0182c26a1751930a3cc28656fb0ba4b
AiSocialNinjaLabStudios <aisocialninjalabstudios@gmail.com>
2025-07-10 00:03:37 -0700

    Create toolbar.js
    @AiSocialNinjaLabStudios

---
+++ src/components/toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Toolbar = () => {
+  return (
+    <div>
+      <button>Free Draw</button>
+      <button>Rectangle</button>
+      <button>Circle</button>
+      <button>Text</button>
+      {/* Add more annotation tools here */}
+    </div>
+  );
+};
+
+export default Toolbar;
